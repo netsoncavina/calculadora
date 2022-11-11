e1f5cae35b0c04af3224bf98625d06d982bd63c9
Netson <netsoncavina@gmail.com>
2022-11-11 00:53:27 -0300

    mudança na pagina raiz da api

--- index.js
+++ index.js
@@ -1,39 +1,17 @@
-// Make a calculator REST API
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import calculosRouter from "./routes/calculos.js";
-// import { add, subtract, multiply, divide } from "./calculator.js";
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  let historico = [
-    {
-      entrada: "2+2",
-      saida: "4",
-      autor: "Netson",
-      data: "2021-03-01",
-    },
-    {
-      entrada: "2-2",
-      saida: "0",
-      autor: "Netson",
-      data: "2021-03-01",
-    },
-    {
-      entrada: "2*2",
-      saida: "4",
-      autor: "Netson",
-      data: "2021-03-01",
-    },
-  ];
-  res.send(historico);
-});
-
 app.use("/calculos", calculosRouter);
+
+app.get("/", (req, res) => {
+  res.send("Para acessar a API, acesse /calculos");
+});
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
